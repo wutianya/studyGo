2cd20fb28e957f28dc37fef13b0ead3dd7dfb93b
wutianya <qw19911018>
2019-04-30 18:14:02 +0800

    update

--- demo_interface/study_interface.go
+++ demo_interface/study_interface.go
@@ -1,10 +1,44 @@
+/*
+定义接口
+type Namer interface {
+	Method1(param_list) return_type
+	Method2(param_list) return_type
+}
+Namer 是接口类型
+*/
+
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+import "fmt"
+
+// interface
+type Shaper interface {
+	Area() float32
+}
+
+// struct
+type Square struct {
+	side float32
+}
+type Rectangle struct {
+	length, width float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
+}
 
 func main() {
-	fmt.Println("cpu number is: ", runtime.NumCPU())
+	q := &Square{5}
+
+	r := Rectangle{5, 3}
+	shapes := []Shaper{r, q}
+	fmt.Println("Looping through shapes for area ...")
+	for n, _ := range shapes {
+		fmt.Println("Shape details: ", shapes[n])
+		fmt.Println("Area of this shape is: ", shapes[n].Area())
+	}
 }
